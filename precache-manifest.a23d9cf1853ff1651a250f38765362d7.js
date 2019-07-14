5eb533904f37ad697c2d0c7745d8688e38a7e338
mpvitalik <mpvitalik@gmail.com>
2019-07-14 22:49:06 +0300

    Updates

---
+++ precache-manifest.a23d9cf1853ff1651a250f38765362d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb34088871979e3cda8ba3b35256b9d",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "cdd92850b639878ca40a",
+    "url": "/react_list-of-goods/static/css/main.ee8e5a85.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/react_list-of-goods/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "cdd92850b639878ca40a",
+    "url": "/react_list-of-goods/static/js/main.55b407ff.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.a23d9cf1853ff1651a250f38765362d7.js"
 );
 
 self.addEventListener('message', (event) => {
